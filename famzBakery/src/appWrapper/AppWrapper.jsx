bb21539967470548c1009a475d559c5febcc75bf
Toheeb <toheenoni9@gmail.com>
2024-12-27 19:22:10 +0100

    Add navigation to the appWrapper

--- famzBakery/src/appWrapper/AppWrapper.jsx
+++ famzBakery/src/appWrapper/AppWrapper.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import './appWrapper.css'
 export default function AppWrapper() {
   return (
-    <div>AppWrapper</div>
+    <div className='app-wrapper'>
+      <nav>
+        <div className="nav-brand">
+          <h2>Famz Bakery</h2>
+        </div>
+        <div className='nav-items'>
+            <ul>
+              <li>Home</li>
+              <li>Blog</li>
+              <li>Shop</li>
+              <li>Contact</li>
+              <li>About</li>
+            </ul>
+        </div>
+        <button className="get-started">
+          Get Started
+        </button>
+      </nav>
+      <main>
+
+      </main>
+    </div>
   )
 }
