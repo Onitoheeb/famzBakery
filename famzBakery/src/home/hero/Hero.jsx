992d493af83816a338230dd6390a7d69be324c16
Toheeb <toheenoni9@gmail.com>
2024-12-28 14:29:01 +0100

    Construct the hero section for the homepage

--- famzBakery/src/home/hero/Hero.jsx
+++ famzBakery/src/home/hero/Hero.jsx
@@ -3,9 +3,12 @@ import './hero.css'
 export default function Hero() {
   return (
     <div className='hero-container'>
+      <div className="hero-wrapper">
         <h1>
           Experience the Warmth and Flavour of Artisan Bread at Our Bakery
         </h1>
+        <button className="hero-button">Explore Flavours</button>
+      </div>
     </div>
   )
 }
