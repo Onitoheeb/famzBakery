17ff8bf7881a58654ab1bff019563a37f400315b
Oni Toheeb <toheeboni9@gmail.com>
2025-01-07 20:05:19 +0100

    Add mobile responsiveness from hero to about section

--- famzBakery/src/home/product/Product.jsx
+++ famzBakery/src/home/product/Product.jsx
@@ -3,30 +3,30 @@ import './product.css'
 import ProductCard from './productCard/ProductCard'
 import breadProducts from './data'
 export default function () {
-  return (
-    <div className='product-container'>
-        <div className="product-header">
+    return (
+        <div className='product-container'>
             <h3>Our Products</h3>
-            <ul>
-                <li>All product</li>
-                <li>Bread</li>
-                <li>Cafe</li>
-                <li>Cookies</li>
-                <li>Others</li>
-            </ul>
+            <div className="product-header">
+                <ul>
+                    <li>All product</li>
+                    <li>Bread</li>
+                    <li>Cafe</li>
+                    <li>Cookies</li>
+                    <li>Others</li>
+                </ul>
+            </div>
+            <div className="product-body">
+                {
+                    breadProducts.length > 0 ? (
+                        breadProducts.map((products) => (
+                            <ProductCard key={products.id} name={products.name} description={products.description} image={products.image} price={products.price} />
+                        ))
+                    )
+                        : (
+                            <p>not found</p>
+                        )
+                }
+            </div>
         </div>
-        <div className="product-body">
-            {
-                breadProducts.length > 0 ? (
-                    breadProducts.map((products) => (
-                        <ProductCard key={products.id} name={products.name} description={products.description} image={products.image} price={products.price} />
-                    ))
-                ) 
-                : (
-                    <p>not found</p>
-                )
-            }
-        </div>
-    </div>
-  )
+    )
 }
